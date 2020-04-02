38a5b665081d09c4e59045413c61af6a081ceb33
Esquilo <vinicius.borba@hotmail.com>
2020-04-02 01:37:06 -0300

    initial commit

---
+++ src/main/java/grupo12/App.java
@@ -0,0 +1,13 @@
+package grupo12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
